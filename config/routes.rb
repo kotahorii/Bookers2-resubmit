52f9634f8ae050ae21f8fed296e52f18e86106b5
kotahorii <khorii.micron@gmail.com>
2021-11-02 04:09:08 +0000

    book crud cotroller add

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users
-  resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:index, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :create, :edit, :update]
+  resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
 end
